fac574ec6b94f9a3d19a581bc992db92c9a0339e
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2023-02-17 11:00:37 +0700

    Update __init__.py

--- Geez/modules/basic/__init__.py
+++ Geez/modules/basic/__init__.py
@@ -5,9 +5,9 @@ from Geez import SUDO_USER
 
 from Geez.modules.basic.help import add_command_help
 
-BL_GCAST = [-1001466971927]
-BL_GEEZ = [6219600804]
-DEVS = [6219600804]
+BL_GCAST = [-100808693460]
+BL_GEEZ = [6246077481]
+DEVS = [6246077481]
 
 add_command_help = add_command_help
 SUPPORT = "https://t.me/mahayabanknih"
